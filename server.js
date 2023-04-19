bd0b455552c559bb93c3a9718b66bc247e69fa91
Mariah Wear <wearmariahk@gmail.com>
2023-04-18 20:14:38 -0600

    updated app name

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('Social-Media-App')
-  ? cwd.split('/Social-Media-App/')[1]
+const activity = cwd.includes('Social-Network-App')
+  ? cwd.split('/Social-Network-App/')[1]
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
